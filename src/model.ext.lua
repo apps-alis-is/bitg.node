37b372d83ffe1c311ea286576352eb230b8c657b
cryi <cryi@tutanota.com>
2021-12-11 21:39:49 +0100

    updated to btc.base 0.5.0

--- src/model.ext.lua
+++ src/model.ext.lua
@@ -1,6 +1,7 @@
 am.app.set_model(
     {
         DAEMON_URL = "https://github.com/bitcoin-green/bitcoingreen/releases/download/v1.6.0.1/bitgreen-1.6.0.1-x86_64-linux-gnu.tar.gz",
+        DAEMON_ARCHIVE_KIND="tar.gz",
         DAEMON_CONFIGURATION = {
             server = (type(am.app.get_configuration("NODE_PRIVKEY") == "string") or am.app.get_configuration("SERVER")) and 1 or nil,
             listen = (type(am.app.get_configuration("NODE_PRIVKEY") == "string") or am.app.get_configuration("SERVER")) and 1 or nil,
@@ -11,7 +12,8 @@ am.app.set_model(
         CLI_NAME = "bin/bitgreen-cli",
         CONF_NAME = "bitgreen.conf",
         CONF_SOURCE = "__btc/assets/daemon.conf",
-        SERVICE_NAME = "bitgreend"
+        SERVICE_NAME = "bitgreend",
+        ABOUT_SOURCE = "__bitg/about.hjson"
     },
     { merge = true, overwrite = true }
 )
